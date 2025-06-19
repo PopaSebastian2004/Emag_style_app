7438b5733687c18d97ae711a7c12c5ea8ac444b0
Sebi <popasebastian1213@gmail.com>
2025-06-19 20:33:18 +0300

    revenire

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,9 +1,5 @@
 const { Pool } = require("pg");
-const { DATABASE_URL } = require("../config/config");
+const { DB } = require("../config/config");
 
-const pool = new Pool({
-    connectionString: DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
-});
-
+const pool = new Pool(DB);
 module.exports = pool;
